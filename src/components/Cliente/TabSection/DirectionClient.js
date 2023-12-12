8e220aa040c2cdb9e0c009b43e57c015f03e95e7
jgrodriguez9 <jgrodriguez.pro@gmail.com>
2023-12-12 00:24:54 -0500

    showing all the countries

--- src/components/Cliente/TabSection/DirectionClient.js
+++ src/components/Cliente/TabSection/DirectionClient.js
@@ -1,93 +1,121 @@
-import { useEffect, useState } from "react";
-import { Col, Input, Label, Row } from "reactstrap";
-import { Country, State, City }  from 'country-state-city';
-import { SELECT_OPTION } from "../../../constants/messages";
-import Select from "react-select";
+import { useEffect, useState } from 'react';
+import { Col, Input, Label, Row } from 'reactstrap';
+import { Country, State, City } from 'country-state-city';
+import { SELECT_OPTION } from '../../../constants/messages';
+import Select from 'react-select';
 
-export default function DirectionClient({formik, item}){
-    const countryOpt = Country.getCountryByCode('MX')
-    const [countryDefault, setCountryDefault] = useState(item ? {label: item?.country, value: item?.country} : null)
-    const [statesOpt, setStatesOpt] = useState([])
-    const [statesDefault, setStatesDefault] = useState(item ? {label: item?.state, value: item?.state} : null)
-    const [citiesOpt, setCitiesOpt] = useState([])
-    const [citiesDefault, setCitiesDefault] = useState(item ? {label: item?.city, value: item?.city} : null)
+export default function DirectionClient({ formik, item }) {
+	const countryOpt = Country.getAllCountries().map((it) => ({
+		label: it.name,
+		value: it.isoCode,
+	}));
+	const [countryDefault, setCountryDefault] = useState(
+		item ? { label: item?.country, value: item?.country } : null
+	);
+	const [statesOpt, setStatesOpt] = useState([]);
+	const [statesDefault, setStatesDefault] = useState(
+		item ? { label: item?.state, value: item?.state } : null
+	);
+	const [citiesOpt, setCitiesOpt] = useState([]);
+	const [citiesDefault, setCitiesDefault] = useState(
+		item ? { label: item?.city, value: item?.city } : null
+	);
 
-    useEffect(() => {
-        if(countryDefault){
-            setStatesOpt(State.getStatesOfCountry(countryDefault.value))
-        }else{
-            setStatesOpt([])
-            setCitiesOpt([])
-        }      
-    },[countryDefault])
+	useEffect(() => {
+		if (countryDefault) {
+			setStatesOpt(State.getStatesOfCountry(countryDefault.value));
+		} else {
+			setStatesOpt([]);
+			setCitiesOpt([]);
+		}
+	}, [countryDefault]);
 
-    useEffect(() => {
-        if(statesDefault){
-            setCitiesOpt(City.getCitiesOfState(countryDefault.value ,statesDefault.value))
-        }else{
-            setCitiesOpt([])
-        }
-    },[statesDefault])
+	useEffect(() => {
+		if (statesDefault) {
+			setCitiesOpt(
+				City.getCitiesOfState(countryDefault.value, statesDefault.value)
+			);
+		} else {
+			setCitiesOpt([]);
+		}
+	}, [statesDefault]);
 
-    return (
-        <Row>
-            <Col xs="12" md="9">
-                <Label htmlFor="address" className="mb-0">Dirección</Label>
-                <Input
-                    id="address"
-                    name="address"
-                    className={`form-control ${formik.errors.address ? 'is-invalid' : ''}`}
-                    onChange={formik.handleChange}
-                    value={formik.values.address}  
-                />
-                {
-                    formik.errors.address &&
-                    <div className="invalid-tooltip">{formik.errors.address}</div>
-                }
-            </Col>
-            <Col xs="12" md="3"></Col>
-            <Col xs="12" md="3">
-                    <Label htmlFor="country" className="mb-0">País</Label>
-                    <Select
-                        value={countryDefault}
-                        onChange={(value) => {
-                            setCountryDefault(value)
-                            formik.setFieldValue('country', value?.label ?? '')
-                            setStatesDefault(null)
-                            setCitiesDefault(null)  
-                        }}
-                        options={[{label: countryOpt.name, value: countryOpt.isoCode}]}
-                        classNamePrefix="select2-selection"
-                        placeholder={SELECT_OPTION}
-                    />
-            </Col> 
-            <Col xs="12" md="3">
-                <Label htmlFor="state" className="mb-0">Estado</Label>
-                <Select
-                    value={statesDefault}
-                    onChange={value => {
-                        setStatesDefault(value)
-                        formik.setFieldValue('state', value?.label ?? '')
-                        setCitiesDefault(null)  
-                    }}
-                    options={statesOpt.map(s=>({label: s.name, value: s.isoCode}))}
-                    classNamePrefix="select2-selection"
-                    placeholder={SELECT_OPTION}
-                />
-            </Col>
-            <Col xs="12" md="3">
-                <Label htmlFor="city" className="mb-0">Ciudad</Label>
-                <Select
-                    value={citiesDefault}
-                    onChange={(value) => {
-                        setCitiesDefault(value)
-                        formik.setFieldValue('city', value?.label ?? '')
-                    }}
-                    options={citiesOpt.map(c=>({label: c.name, value:c.isoCode}))}
-                    classNamePrefix="select2-selection"
-                    placeholder={SELECT_OPTION}
-                />
-            </Col>   
-        </Row>
-    )
+	return (
+		<Row>
+			<Col xs="12" md="9">
+				<Label htmlFor="address" className="mb-0">
+					Dirección
+				</Label>
+				<Input
+					id="address"
+					name="address"
+					className={`form-control ${
+						formik.errors.address ? 'is-invalid' : ''
+					}`}
+					onChange={formik.handleChange}
+					value={formik.values.address}
+				/>
+				{formik.errors.address && (
+					<div className="invalid-tooltip">
+						{formik.errors.address}
+					</div>
+				)}
+			</Col>
+			<Col xs="12" md="3"></Col>
+			<Col xs="12" md="3">
+				<Label htmlFor="country" className="mb-0">
+					País
+				</Label>
+				<Select
+					value={countryDefault}
+					onChange={(value) => {
+						setCountryDefault(value);
+						formik.setFieldValue('country', value?.label ?? '');
+						setStatesDefault(null);
+						setCitiesDefault(null);
+					}}
+					options={countryOpt}
+					classNamePrefix="select2-selection"
+					placeholder={SELECT_OPTION}
+				/>
+			</Col>
+			<Col xs="12" md="3">
+				<Label htmlFor="state" className="mb-0">
+					Estado
+				</Label>
+				<Select
+					value={statesDefault}
+					onChange={(value) => {
+						setStatesDefault(value);
+						formik.setFieldValue('state', value?.label ?? '');
+						setCitiesDefault(null);
+					}}
+					options={statesOpt.map((s) => ({
+						label: s.name,
+						value: s.isoCode,
+					}))}
+					classNamePrefix="select2-selection"
+					placeholder={SELECT_OPTION}
+				/>
+			</Col>
+			<Col xs="12" md="3">
+				<Label htmlFor="city" className="mb-0">
+					Ciudad
+				</Label>
+				<Select
+					value={citiesDefault}
+					onChange={(value) => {
+						setCitiesDefault(value);
+						formik.setFieldValue('city', value?.label ?? '');
+					}}
+					options={citiesOpt.map((c) => ({
+						label: c.name,
+						value: c.isoCode,
+					}))}
+					classNamePrefix="select2-selection"
+					placeholder={SELECT_OPTION}
+				/>
+			</Col>
+		</Row>
+	);
 }
--- src/utils/roles.js
+++ src/utils/roles.js
@@ -1,18 +1,6 @@
 export const existsRole = (roles, rolesToCheck) => {
-	const rolesList = [
-		'ROLE_Administracion',
-		'ROLE_COMPANY_ADMIN',
-		'ROLE_OPERATIONS',
-		'ROLE_ACCOUNTING',
-		'ROLE_MANAGER',
-		'ROLE_TOUR',
-	];
-
 	if (!rolesToCheck) return true;
 	if (!roles) return true;
-
-	console.log(roles);
-	console.log(rolesToCheck);
 
 	return roles.every((r) => {
 		if (rolesToCheck.includes(r)) {
